ab7926f21ebe9f9139d8489165fc7d54bdb49e56
Paiva Dias Luca tapaivad <luca.paivadias@stud.hslu.ch>
2019-05-06 08:20:38 +0200

    added communicator.ts (non working)

--- src/app/services/communicator.ts
+++ src/app/services/communicator.ts
@@ -1,9 +1,12 @@
 import { Injectable } from '@angular/core'
 
+
 @Injectable()
 export class Communicator {
     
     constructor(){
-
+    
     }
 }
+
+//steal this bro: https://medium.com/javascript-everyday/singleton-made-easy-with-typescript-6ad55a7ba7ff
--- src/app/settings/settings.page.ts
+++ src/app/settings/settings.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { NavController } from '@ionic/angular'
+import { CordovaMqTTPlugin } from 'cordova-plugin-mqtt'
 
 @Component({
   selector: 'app-settings',
@@ -8,7 +9,9 @@ import { NavController } from '@ionic/angular'
 })
 export class SettingsPage implements OnInit {
 
-  constructor(public navCtrl: NavController) { }
+  constructor(public navCtrl: NavController) { 
+    
+  }
 
   ngOnInit() {
   }
@@ -22,6 +25,39 @@ export class SettingsPage implements OnInit {
 
     //this.dataList = reorderArray(this.dataList, ev.detail.from, ev.detail.to);
     ev.detail.complete();
+  }
+
+  testMQTT(){
+    cordova.plugins.CordovaMqTTPlugin.connect({
+      url:"tcp://test.mosquitto.org", //a public broker used for testing purposes only. Try using a self hosted broker for production.
+      port:1883,
+      clientId:"YOUR_USER_ID_LESS_THAN_24_CHARS",
+      connectionTimeout:3000,
+      willTopicConfig:{
+          qos:0, //default is 0
+          retain:true, //default is true
+          topic:"<will topic>",
+          payload:"<will topic message>"
+      },
+      username:"uname",
+      password:'pass',
+      keepAlive:60,
+      isBinaryPayload: false, //setting this 'true' will make plugin treat all data as binary and emit ArrayBuffer instead of string on events
+      success:function(s){
+          console.log("connect success");
+      },
+      error:function(e){
+          console.log("connect error");
+      },
+      onConnectionLost:function (){
+          console.log("disconnect");
+      },
+      routerConfig:{
+          router:routerObject //instantiated router object
+          publishMethod:"emit", //refer your custom router documentation to get the emitter/publishing function name. The parameter should be a string and not a function.
+          useDefaultRouter:false //Set false to use your own topic router implementation. Set true to use the stock topic router implemented in the plugin.
+      }
+  })
   }
 
 }
